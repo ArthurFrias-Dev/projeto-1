e2f9c07088a8646a81f02000185f8b6882063361
ArthurFrias-Dev <165163090+ArthurFrias-Dev@users.noreply.github.com>
2026-03-24 13:33:03 +0000

    criação de diretorio de exercicios

---
+++ Exercicios/calculadora.js
@@ -0,0 +1,20 @@
+//Exercicios de node.js da alura
+
+let numero1 = 4
+
+let numero2 = 2
+
+const multiplicacao = numero1 * numero2
+
+const subtracao = numero1 - numero2
+
+const adicao = numero1 + numero2
+
+const divisao = numero1 / numero2
+
+console.log(multiplicacao)
+console.log(subtracao)
+console.log(adicao)
+console.log(divisao)
+
+
